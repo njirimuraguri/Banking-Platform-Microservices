92e4b8356cc8cdf8ac11975a011bc79bfc2c58a6
Njiri Muraguri <njirimuraguri@gmail.com>
2025-05-15 19:17:31 +0300

    Add account-service and card-service microservices

---
+++ card-service/src/test/java/com/banking/card_service/CardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.card_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
